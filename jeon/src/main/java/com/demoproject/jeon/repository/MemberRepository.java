fa4f9ce8d08d062133e67ad47a25751cd5e543b8
Jeon03 <jeonyu2589@naver.com>
2025-03-30 19:29:40 +0900

    Delete jeon directory

--- jeon/src/main/java/com/demoproject/jeon/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demoproject.jeon.repository;
-
-import com.demoproject.jeon.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-}
