239975d78eaf9f123bbf34424bc78b084cb8c174
Jeon03 <jeonyu2589@naver.com>
2025-04-01 01:46:56 +0900

    Add files via upload

---
+++ ShareStory/src/main/java/com/dream/ShareStory/ShareStoryApplication.java
@@ -0,0 +1,13 @@
+package com.dream.ShareStory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareStoryApplication.class, args);
+	}
+
+}
