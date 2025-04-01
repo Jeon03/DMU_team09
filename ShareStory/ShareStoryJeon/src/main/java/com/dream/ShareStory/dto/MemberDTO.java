5e9dfe0249e7c7abd5425d061706a5459586fa0a
Jeon03 <jeonyu2589@naver.com>
2025-04-02 00:13:54 +0900

    Add files via upload

---
+++ ShareStory/ShareStoryJeon/src/main/java/com/dream/ShareStory/dto/MemberDTO.java
@@ -0,0 +1,28 @@
+package com.dream.ShareStory.dto;
+
+
+import com.dream.ShareStory.entity.MemberEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor  //기본생성자 자동생성
+@ToString
+public class MemberDTO {
+    private Long id;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity){
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        return memberDTO;
+    }
+}
---
+++ ShareStory/ShareStoryJeon/src/main/java/com/dream/ShareStory/service/MemberService.java
@@ -0,0 +1,88 @@
+package com.dream.ShareStory.service;
+
+
+import com.dream.ShareStory.dto.MemberDTO;
+import com.dream.ShareStory.entity.MemberEntity;
+import com.dream.ShareStory.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    public void signup(MemberDTO memberDTO) {
+        //1. dto -> entity 변환
+        //2. repository의 save 메서드 호출
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        memberRepository.save(memberEntity);
+        //repository의 save메서드 호출(조건. entity 객체를 넘겨줘야함)
+
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+        /*
+            1.회원이 입력한 이메일로 DB에서 조회
+            2.DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판단
+         */
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if(byMemberEmail.isPresent()){
+            //조회 결과가 있다(해당이메일을 가진 회원 정보가 있다)
+            MemberEntity memberEntity = byMemberEmail.get();
+            if(memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())){
+                //비밀번호가 일치
+                //entity -> dto 변환후 리턴
+                MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
+                return dto;
+            }
+            else{
+                //비밀번호 불일치
+                return null;
+            }
+        }
+        else{
+            return null;
+        }
+    }
+
+    public List<MemberDTO> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        for(MemberEntity memberEntity : memberEntityList){
+            memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
+        }
+        return memberDTOList;
+    }
+
+    public MemberDTO findbyId(Long id) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
+        if(optionalMemberEntity.isPresent()){
+            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+        }
+        else{
+            return null;
+        }
+    }
+
+    public MemberDTO updateForm(String myEmail) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(myEmail);
+        if (optionalMemberEntity.isPresent()) {
+            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public void update(MemberDTO memberDTO) {
+        memberRepository.save(MemberEntity.toUpdateMemberEntity(memberDTO));
+    }
+
+    public void deleteById(Long id) {
+        memberRepository.deleteById(id);
+    }
+}
